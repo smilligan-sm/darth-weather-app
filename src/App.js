5d8f5a9cc97a9f580d71a953e25a638d35fb0f9f
smilligan-sm <sarahlouisemilligan.sm@gmail.com>
2025-06-03 16:18:03 +1000

    Updated footer links

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ export default function App() {
       <div className="container">
       <Weather defaultCity="Canberra" />
       <footer>
-      Built by <a href="https://github.com/smilligan-sm" target="_blank" rel="noreferrer">Sarah Milligan</a>, is open-sourced on <a href="https://github.com/smilligan-sm/sm-react-weather-app" target="_blank" rel="noreferrer">Github</a
-      > and hosted on <a href="https://tiny-concha-66febd.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>
+      Built by <a href="https://github.com/smilligan-sm" target="_blank" rel="noreferrer">Sarah Milligan</a>, is open-sourced on <a href="https://github.com/smilligan-sm/darth-weather-app" target="_blank" rel="noreferrer">Github</a
+      > and hosted on <a href="https://sm-darth-weather.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>
     </footer>
     </div>
     </div>
