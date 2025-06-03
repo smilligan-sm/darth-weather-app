c5a6bbfe0e3be850c627f7f9298d4671036dcbbb
smilligan-sm <sarahlouisemilligan.sm@gmail.com>
2025-06-03 16:55:48 +1000

    Updated background image and fixed padding/margin properties
    When screen is zoomed out, black background will replace the area not covered by the image. Footer had been set to be fixed to the bottom part of the image.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from "react";
 import Weather from "./Weather";
 import './App.css';
 
+
+
 export default function App() {
   return (
+    <div className="app-background">
     <div className="App">
       <div className="container">
       <Weather defaultCity="Canberra" />
@@ -11,6 +14,7 @@ export default function App() {
       Built by <a href="https://github.com/smilligan-sm" target="_blank" rel="noreferrer">Sarah Milligan</a>, is open-sourced on <a href="https://github.com/smilligan-sm/darth-weather-app" target="_blank" rel="noreferrer">Github</a
       > and hosted on <a href="https://sm-darth-weather.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>
     </footer>
+    </div>
     </div>
     </div>
   );
